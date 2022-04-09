f13e0a59b7c8aa536b731ab7202bf11c6bc5eb1b
JasonAHendry <jason.ahendry@gmail.com>
2022-04-09 12:59:05 +0200

    Updated CLI to use CLICK.

---
+++ src/nomadic/pipeline/cli.py
@@ -0,0 +1,95 @@
+import click
+
+
+def common_options(fn):
+    fn = click.option(
+        "-b", "--barcode",
+        type=int,
+        help="Optionally run command for only a single barcode.")(fn)
+    fn = click.option(    
+        "-c", "--config", 
+        type=str, 
+        default="configs/default.ini", 
+        help="Path to NOMADIC configuration (.ini) file.")(fn)
+    fn = click.option(
+        "-e", "--expt_dir", 
+        type=str, 
+        required=True, 
+        help="Path to experiment directory.")(fn)
+    return fn
+    
+
+# I think this is where I set logging verbosity
+# Then we want that verbosity level to pass to the sub-modules, probably
+@click.group()
+def cli():
+    """
+    NOMADIC: A pipeline for analysis of malaria long-read data
+    
+    """
+    pass
+
+@cli.command(short_help="Run complete pipeline.")
+@common_options
+def runall(expt_dir, config, barcode):
+    """
+    Run the complete NOMADIC pipeline
+    
+    """
+    from nomadic.pipeline import map_pf, remap_to_hs
+    map_pf.main(expt_dir, config, barcode)
+    remap_to_hs.main(expt_dir, config, barcode)
+
+@cli.command(short_help="Map to P.f. reference.")
+@common_options
+def map(expt_dir, config, barcode):
+    """
+    Map .fastq files to the P. falciparum reference genome
+    
+    """
+    from nomadic.pipeline import map_pf
+    map_pf.main(expt_dir, config, barcode)
+
+@cli.command(short_help="Map unmapped reads to H.s.")
+@common_options
+def remap(expt_dir, config, barcode):
+    """
+    Some reads may fail to map to the P.f. reference genome after
+    running `nomadic map`
+
+    `nomadic remap` tries to map these reads to the H.s. reference
+    genome
+    
+    """
+    from nomadic.pipeline import remap_to_hs
+    remap_to_hs.main(expt_dir, config, barcode)
+
+@cli.command(short_help="QC analysis of .bam files.")
+@common_options
+def qcbams(expt_dir, config, barcode):
+    """
+    Run a quality control analysis of .bam files generated from
+    `nomadic map` and `nomadic remap`
+    
+    """
+    from nomadic.pipeline import qc_bams
+    qc_bams.main(expt_dir, config, barcode)
+
+
+@cli.command(short_help="Analyse amplicon targets")
+@common_options
+def targets(expt_dir, config, barcode):
+    """
+    Run analyses of a specific set of amplicon targets
+
+    TODO:
+    - Probably want an additional argument here specifying targets
+    - Would add flexibility
+    
+    """
+    from nomadic.pipeline import target_extraction
+    target_extraction.main(expt_dir, config, barcode)
+
+
+if __name__ == "__main__":
+    cli()
