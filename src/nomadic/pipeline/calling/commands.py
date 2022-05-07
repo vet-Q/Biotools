192b17e17b2598355bed900b8bf2a0ab2a9455f6
JasonAHendry <jason.ahendry@gmail.com>
2022-05-07 11:53:38 +0200

    Added functionality for downsampling using --downsample flag.

--- src/nomadic/pipeline/calling/commands.py
+++ src/nomadic/pipeline/calling/commands.py
@@ -4,6 +4,7 @@ from nomadic.lib.generic import print_header, print_footer, produce_dir
 from nomadic.lib.parsing import build_parameter_dict
 from nomadic.lib.references import PlasmodiumFalciparum3D7
 from nomadic.pipeline.calling.callers import caller_collection
+from .downsample import BamDownSampler
 
 
 @click.command(short_help="Call variants across targets.")
@@ -17,19 +18,49 @@ from nomadic.pipeline.calling.callers import caller_collection
     help="Variant calling method to use.",
 )
 @click.option(
-    "--downsample", is_flag=True, help="Produce an overview across all barcodes."
+    "--downsample", 
+    is_flag=True, 
+    help="Produce an overview across all barcodes."
 )
-def call(expt_dir, config, barcode, method, downsample):
+@click.option(
+    "-r",
+    "--reads",
+    type=int,
+    default=None,
+    multiple=True, # should allow setting multiple read depths by repeating the flag
+    help="If --downsample invoked, number of reads to downsample to. Can be passed multiple times, e.g. -r 10 -r 50 -r 100."
+)
+@click.option(
+    "-i",
+    "--iterations",
+    type=int,
+    default=10,
+    help="If --downsample invoked, number of times to iterate downsampling for each number of reads."
+)
+def call(expt_dir, config, barcode, method, downsample, reads, iterations):
     """
-    Run analyses of a specific set of amplicon targets
-
-    TODO:
-    - Probably want an additional argument here specifying targets
-    - Would add flexibility
-
+    Call variants, optionally with downsampling.
+    
     """
+    
+    # Downsampling
+    if downsample:
+        # Check a number of reads have bee passed
+        if reads is None:
+            raise ValueError("If --dowsample invoked, musts pass interger to -r.")
+
+        # Run
+        call_with_downsample(expt_dir, config, barcode, method, reads, iterations)
+        
+    # No dowampling
+    else:
+        call_all_reads(expt_dir, config, barcode, method)
+
+
+def call_all_reads(expt_dir, config, barcode, method):
+    """ Call variants across all reads """
     # PARSE INPUTS
-    script_descrip = "NOMADIC: Map .fastq files to Plasmodium falciparum"
+    script_descrip = "NOMADIC: Call variants without downsampling"
     t0 = print_header(script_descrip)
     script_dir = f"calling/{method}"  # this will be easier to iterate overs
     params = build_parameter_dict(expt_dir, config, barcode)
@@ -74,72 +105,155 @@ def call(expt_dir, config, barcode, method, downsample):
     print_footer(t0)
 
 
-def call_with_downsample(expt_dir, config, barcode):
-    """
-    Call variants for a P.f. sample
-
-    TODO:
-    - INCOMPLETE
-
-    Steps
-    - Load params
-    - Run with or without downsampling;
-    - Call variants across all extracted targets
-
+def call_with_downsample(expt_dir, config, barcode, method, reads, iterations):
     """
 
+    """
     # PARSE INPUTS
-    script_descrip = "NOMADIC: Map .fastq files to Plasmodium falciparum"
+    script_descrip = "NOMADIC: Call variants without downsampling"
     t0 = print_header(script_descrip)
-    script_dir = "calling"
+    script_dir = produce_dir("calling", method, "downsample")  # this will be easier to iterate overs
     params = build_parameter_dict(expt_dir, config, barcode)
+
+    # Parse reads
+    print(f"Number of reads to downsample to: {reads}")
+
+    # Define reference genome
+    reference = PlasmodiumFalciparum3D7()
 
     # Focus on a single barcode, if specified
     if "focus_barcode" in params:
         params["barcodes"] = [params["focus_barcode"]]
 
-    # Define reference genomes
-    references = [
-        PlasmodiumFalciparum3D7(),
-    ]
-
     # ITERATE over barcodes
     for barcode in params["barcodes"]:
-
         # Define input and output directory
-        print("-" * 80)
-        print(f"Running longshot for: {barcode}")
+        print("." * 80)
+        print(f"Running {method} for: {barcode}")
+        print("." * 80)
         barcode_dir = f"{params['barcodes_dir']}/{barcode}"
         input_dir = f"{barcode_dir}/target-extraction"
         output_dir = produce_dir(barcode_dir, script_dir)
 
-        # ITERATE over targets
+        # ITERATE over reads
         for target_gene in params["target_names"]:
-
+            print(f"Target: {target_gene}")
             # Define input bam
             bam_path = f"{input_dir}/reads.target.{target_gene}.bam"
+            for n_reads in reads:
+                # Instantiate downsampler; ensure enough reads
+                bam_manager = BamDownSampler(bam_path)
+                if bam_manager.n_reads_total < n_reads:
+                    print(f"Not enough reads for {target_gene} to downsample.")
+                    print(f"  No. reads total: {bam_manager.n_reads_total}")
+                    print(f"  No. downsampling: {n_reads}")
+                    continue
+                
+                for ix in range(iterations):
 
-            # Instantiate downsampler; ensure enough reads
-            bam_manager = BamDownSampler(bam_path)
-            if bam_manager.n_reads_total < args.downsample_reads:
-                print(f"Not enough reads for {target_gene} to downsample.")
-                print(f"  No. reads total: {bam_manager.n_reads_total}")
-                print(f"  No. downsampling: {args.downsample_reads}")
-                continue
+                    # Create dowsample
+                    print("Downsampling...")
+                    bam_manager.create_downsample(n_reads)
+                    print("Done.")
+                    print("")
 
-            # ITERATE over replicates
-            for ix in range(args.iterations):
+                    # Define output vcf
+                    vcf_fn = f"{barcode}.{target_gene}."
+                    vcf_fn += f"n{n_reads:04d}.r{ix:03d}.vcf"
+                    vcf_path = f"{output_dir}/{vcf_fn}"
 
-                # Create dowsample
-                bam_manager.create_downsample(args.downsample_reads)
+                    # Select variant calling method
+                    print("Calling variants...")
+                    print(f"  Input: {bam_path}")
+                    print(f"  Ouput: {vcf_path}")
+                    caller = caller_collection[method]
+                    caller.set_files(
+                        bam_path=bam_manager.downsampled_bam_path,  # here we pass downsampled .bam
+                        vcf_path=vcf_path,
+                    )
+                    caller.set_arguments(fasta_path=reference.fasta_path)
+                    caller.call_variants()
+                    print("Done.")
+                    print("")
 
-                # Define output vcf
-                vcf_fn = f"{args.barcode}.{target_gene}."
-                vcf_fn += f"n{args.downsample_reads:04d}.r{ix:03d}.vcf"
-                vcf_path = f"{output_dir}/{vcf_fn}"
+                    # Remove downsampled bam
+                    bam_manager.remove_downsample()
+    print_footer(t0)
 
-                # Select variant calling method
 
-                # Run
 
-                # Remove downsample
+
+
+
+
+
+
+
+
+# @click.group(short_help="Call variants across targets.")
+# @experiment_options
+# @barcode_option
+# @click.option(
+#     "-m",
+#     "--method",
+#     type=click.Choice(caller_collection),
+#     required=True,
+#     help="Variant calling method to use.",
+# )
+# @click.option(
+#     "--downsample", is_flag=True, help="Produce an overview across all barcodes."
+# )
+# def call(expt_dir, config, barcode, method, downsample):
+#     """
+#     Run analyses of a specific set of amplicon targets
+
+#     TODO:
+#     - Probably want an additional argument here specifying targets
+#     - Would add flexibility
+
+#     """
+#     # PARSE INPUTS
+#     script_descrip = "NOMADIC: Map .fastq files to Plasmodium falciparum"
+#     t0 = print_header(script_descrip)
+#     script_dir = f"calling/{method}"  # this will be easier to iterate overs
+#     params = build_parameter_dict(expt_dir, config, barcode)
+
+#     # Define reference genome
+#     reference = PlasmodiumFalciparum3D7()
+
+#     # Focus on a single barcode, if specified
+#     if "focus_barcode" in params:
+#         params["barcodes"] = [params["focus_barcode"]]
+
+#     # ITERATE over barcodes
+#     for barcode in params["barcodes"]:
+
+#         # Define input and output directory
+#         print("-" * 80)
+#         print(f"Running {method} for: {barcode}")
+#         barcode_dir = f"{params['barcodes_dir']}/{barcode}"
+#         input_dir = f"{barcode_dir}/target-extraction"
+#         output_dir = produce_dir(barcode_dir, script_dir)
+
+#         # ITERATE over targets
+#         for target_gene in params["target_names"]:
+
+#             # Define input bam and output vcf
+#             bam_path = f"{input_dir}/reads.target.{target_gene}.bam"
+#             vcf_path = f"{output_dir}/reads.target.{target_gene}.vcf"
+
+#             # Select variant calling method
+#             print("Calling variants...")
+#             print(f"  Input: {bam_path}")
+#             print(f"  Ouput: {vcf_path}")
+#             caller = caller_collection[method]
+#             caller.set_files(
+#                 bam_path=bam_path,
+#                 vcf_path=vcf_path,
+#             )
+#             caller.set_arguments(fasta_path=reference.fasta_path)
+#             caller.call_variants()
+#             print("Done.")
+#             print("")
+#     print_footer(t0)
+
