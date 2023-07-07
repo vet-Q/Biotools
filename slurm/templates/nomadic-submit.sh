eaf917da61e4b99621133cbef11045d7ac87ee04
JasonAHendry <jason.ahendry@gmail.com>
2023-07-07 12:46:21 +0200

    Fix bug, first script should be guppy.

--- slurm/templates/nomadic-submit.sh
+++ slurm/templates/nomadic-submit.sh
@@ -3,6 +3,6 @@
 # via Slurm
 # JHendry, 2023/06/19
 
-gup_jid=$(sbatch --parsable {run_dir}/nomadic-barcode.sh)
+gup_jid=$(sbatch --parsable {run_dir}/nomadic-guppy.sh)
 bar_jid=$(sbatch --parsable --dependency=afterany:$gup_jid {run_dir}/nomadic-barcode.sh)
 expt_jid=$(sbatch --parsable --dependency=afterany:$bar_jid {run_dir}/nomadic-experiment.sh)
