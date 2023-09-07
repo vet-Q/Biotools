89ff255d8e3be24fd5a420e09dea3f899b644cee
JasonAHendry <jason.ahendry@gmail.com>
2023-09-07 18:06:10 +0200

    Run guppy via nomadic.

--- slurm/templates/pipelines/nomadic-guppy.sh
+++ slurm/templates/pipelines/nomadic-guppy.sh
@@ -14,53 +14,28 @@
 #
 #SBATCH --time=24:00:00
 
+
 # SETTINGS
 expt_dir={expt_dir}
 config={config}
+basecalling_method={basecalling_method}
+barcode_kit={barcoding_strategy}
 
-# SETTINGS
-# Directories
-POD5_DIR=$expt_dir"/minknow/pod5_pass"
-FASTQ_DIR=$expt_dir"/guppy/hac/fastq/pass"
-DEMUX_FASTQ_DIR=$expt_dir"/guppy/hac/single_end"
-
-mkdir -p $FASTQ_DIR
-mkdir -p $DEMUX_FASTQ_DIR
-
-# Parameters
-BASECALL_SETTINGS="dna_r10.4.1_e8.2_400bps_hac.cfg"
-BARCODE_KIT="SQK-NBD114-96"
-
-# LOAD ANACONDA & ACTIVATE
+# LOAD ANACONDA
 module load anaconda/3/.2023.03
+
+# ACTIVATE ENVIRONMENT
 conda activate nomadic2-fast
 
-# RUN BASECALLING
-echo "Runninng basecalling..."
-guppy_basecaller \
---device 'cuda:0' \
---min_qscore 8 \
---compress_fastq \
---config $BASECALL_SETTINGS \
---input_path $POD5_DIR \
---recursive \
---save_path $FASTQ_DIR \
---disable_pings
-echo "Done."
-echo ""
+# RUN NOMADIC
+nomadic basecall \
+-e $expt_dir \
+-f R10 \
+-m $basecalling_method \
+-q 12
 
-# RUN BARCODING
-echo "Runninng barcoding..."
-guppy_barcoder \
---device 'cuda:0' \
---recursive \
---compress_fastq \
---enable_trim_barcodes \
---trim_adapters \
---input_path $FASTQ_DIR \
---save_path $DEMUX_FASTQ_DIR \
---disable_pings \
-#--require_barcodes_both_ends
-echo "Done."
-echo ""
+nomadic barcode \
+-e $expt_dir \
+-m $basecalling_method \
+-k $barcode_kit {barcode_strict} {require_both_ends}
 
