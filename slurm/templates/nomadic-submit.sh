6c23fe163c9646168e6624b83685bb69f368c382
JasonAHendry <jason.ahendry@gmail.com>
2023-09-07 18:08:41 +0200

    Only re-run barcoding if guppy has run successfully.

--- slurm/templates/nomadic-submit.sh
+++ slurm/templates/nomadic-submit.sh
@@ -4,5 +4,5 @@
 # JHendry, 2023/06/19
 
 gup_jid=$(sbatch --parsable {run_dir}/nomadic-guppy.sh)
-bar_jid=$(sbatch --parsable --dependency=afterany:$gup_jid {run_dir}/nomadic-barcode.sh)
+bar_jid=$(sbatch --parsable --dependency=afterok:$gup_jid {run_dir}/nomadic-barcode.sh)
 expt_jid=$(sbatch --parsable --dependency=afterany:$bar_jid {run_dir}/nomadic-experiment.sh)
